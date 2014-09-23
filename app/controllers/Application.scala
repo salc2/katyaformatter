698c8b90e2da3a325d72498a4e10d66a6bd4ff39
javier salcedo <javiersalcedo75@gmail.com>
2014-09-23 11:59:25 -0300

    commit arg

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,7 +5,6 @@ import play.api.mvc._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import scala.util.{Failure, Success}
 
 object Application extends Controller {
 
@@ -24,13 +23,10 @@ object Application extends Controller {
       val contentType = csv.contentType
       val source = scala.io.Source.fromFile(csv.ref.file)
 
-      val lines = source.getLines
-      lines foreach (x => {
-        formatPretty(x) onComplete {
-          case Success(o) => println(o+"\n")
-          case Failure(t) => println("Error!")
-        }
-      } )
+     source.getLines.toList map(x => {
+       B ++= formatPretty(x)
+     })
+
 
       source.close()
       B.toString().getBytes
@@ -43,25 +39,25 @@ object Application extends Controller {
 
   def formatPretty(bigline:String):Future[String] ={
     val id = bigline.split(",")(0)
-    val desc = bigline.split(",")(1)
+    val desc = bigline.split(",")(1).replace(".","_").replace(" ","_")
 
     def conver2Letters(line:String):String = {
       val pattern = "^[ña-zÑA-Z]+$"
-      val splitL = line.split(" ")
-      def choice(l:List[String],total:String):String = {
-        if(l.tail.isEmpty) total else if(l.head matches(pattern)) choice(l.tail,total.trim+" "+l.head)
+      val splitL = line.split("_")
+      def choice(l:List[String],total:List[String]):String = {
+        if(l.isEmpty) total.mkString(" ") else if(l.head matches(pattern)) choice(l.tail,total:+l.head)
         else{ choice(l.tail,total)}}
-      choice(splitL.toList,"").toUpperCase()
+      choice(splitL.toList,List.empty).toUpperCase()
     }
 
     val out = conver2Letters(desc)
-
-    WS.url(s"http://translate.google.com/translate_a/t?client=t&text=$out&hl=pt&sl=pt&tl=en&multires=1&otf=2&pc=1&ssel=0&tsel=0&sc=1".replace(" ","%20").replace("|","%7C")).get().map{
+    s"X=$id;#$id"+"_"+out
+   /* WS.url(s"http://translate.google.com/translate_a/t?client=t&text=$out&hl=pt&sl=pt&tl=en&multires=1&otf=2&pc=1&ssel=0&tsel=0&sc=1".replace(" ","%20").replace("|","%7C")).get().map{
       //f => s"X=$id;#$id"+"_"+(f.json \ "responseData" \ "translatedText").as[String]
       f => {
         s"X=$id;#$id"+"_"+f.ahcResponse.getResponseBody.replace("[","").replace("]","").split(",")(0).replace("\"","").toUpperCase()
       }
-    }
+    }*/
 
   }
   /*def formatPretty(bigline:String):Future[String] ={
--- app/testingcsv.sc
+++ app/testingcsv.sc
@@ -7,23 +7,24 @@ val li = "33,  *  zzENetPlc[6].TimeoutFault  Falha de TimeOut Plc 06 Remoto     
 
 def formatPretty(bigline:String):Future[String] ={
   val id = bigline.split(",")(0)
-  val desc = bigline.split(",")(1)
+  val desc = bigline.split(",")(1).replace(".","_").replace(" ","_")
 
   def conver2Letters(line:String):String = {
     val pattern = "^[ña-zÑA-Z]+$"
-    val splitL = line.split(" ")
-    def choice(l:List[String],total:String):String = {
-      if(l.tail.isEmpty) total else if(l.head matches(pattern)) choice(l.tail,total.trim+" "+l.head)
+    val splitL = line.split("_")
+    def choice(l:List[String],total:List[String]):String = {
+      if(l.isEmpty) total.mkString(" ") else if(l.head matches(pattern)) choice(l.tail,total:+l.head)
       else{ choice(l.tail,total)}}
-    choice(splitL.toList,"").toUpperCase()
+    choice(splitL.toList,List.empty).toUpperCase()
   }
 
   val out = conver2Letters(desc)
 
   WS.url(s"http://translate.google.com/translate_a/t?client=t&text=$out&hl=pt&sl=pt&tl=en&multires=1&otf=2&pc=1&ssel=0&tsel=0&sc=1".replace(" ","%20").replace("|","%7C")).get().map{
     //f => s"X=$id;#$id"+"_"+(f.json \ "responseData" \ "translatedText").as[String]
-    f => f.ahcResponse.getResponseBody
-      ""
+    f => {
+      s"X=$id;#$id"+"_"+f.ahcResponse.getResponseBody.replace("[","").replace("]","").split(",")(0).replace("\"","").toUpperCase()
+    }
   }
 
 }
