1a7c3eeb5b483da875a82c6f1bb265eb83fecb8e
javier salcedo <javiersalcedo75@gmail.com>
2014-09-24 22:04:05 -0300

    so far so good

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,8 +5,7 @@ import play.api.mvc._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import scala.util.{Failure, Success}
-import scalax.io.{Resource, Output}
+import scalax.io.{Output, Resource}
 
 object Application extends Controller {
 
@@ -30,29 +29,32 @@ object Application extends Controller {
 
 
     val csv =request.body.file("csv").get
-    val output:Output = Resource.fromFile("readys/ready-"+csv.filename)
+    val output:Output = Resource.fromFile("/tmp/ready-"+csv.filename)
       val futu = scala.concurrent.future{
         val B = new StringBuilder
         val filename = csv.filename
         val contentType = csv.contentType
         val source = scala.io.Source.fromFile(csv.ref.file)(io.Codec.ISO8859)
         source.getLines.toList map(x => {
-          formatPretty(x) onComplete {
-            case Success(l) => if(!l.trim.isEmpty){ output.write( l+"\n")(scalax.io.Codec.UTF8)}
-            case Failure(l) => println("Error")
+          formatPretty(x) onSuccess {
+            case l => output.write( l+"\n")(scalax.io.Codec.ISO8859)
           }
+
         })
         source.close()
         B.toString()
       }
       futu map {
-      x => Ok("ready")
+      x => Ok.sendFile(
+          content = new java.io.File("/tmp/ready-"+csv.filename),
+          fileName = _ => "readys/ready-"+csv.filename
+      )
     }
 
   }
 
 
-  def formatPretty(biglineIn:String):Future[String] ={
+/*  def formatPretty(biglineIn:String):Future[String] ={
     if (biglineIn.split(";").length > 1){
       val bigline =if(biglineIn.split(";").length==1) biglineIn+"FAULT" else biglineIn
     val id = bigline.split(";")(0)
@@ -79,7 +81,42 @@ object Application extends Controller {
     }else{
       Future("")
     }
+  }*/
+
+  def formatPretty(biglineIn:String):Future[String]  ={
+    if (biglineIn.split(";").length > 1){
+      val bigline =if(biglineIn.split(";").length==1) biglineIn+"FAULT" else biglineIn
+      val id = bigline.split(";")(0)
+      val desc = bigline.split(";")(1).replace(".","_").replace(" ","_")
+
+      def conver2Letters(line:String):String = {
+        val pattern = "^[ña-zÑA-Z]+$"
+        val splitL = line.replaceAll("[^ña-zÑA-Z]"," ").replace(" ","_").split("_")
+        def choice(l:List[String],total:List[String]):String = {
+          if(l.isEmpty) total.mkString(" ") else if(l.head matches(pattern)) choice(l.tail,total:+l.head)
+          else{ choice(l.tail,total)}}
+        choice(splitL.toList,List.empty).toUpperCase()
+      }
+
+      val o = conver2Letters(desc)
+      val out = if(o.trim.isEmpty) "FAULT" else o
+      //s"X=$id;#$id"+"_"+out
+      val futu = WS.url(s"http://translate.google.com/translate_a/t?client=t&text=$out&hl=pt&sl=pt&tl=en&multires=1&otf=2&pc=1&ssel=0&tsel=0&sc=1".replace(" ","%20").replace("|","%7C")).get().map{
+        //f => s"X=$id;#$id"+"_"+(f.json \ "responseData" \ "translatedText").as[String]
+        f => {
+          if(id.isEmpty) println("Vacio: "+biglineIn)
+          "X="+id+";#"+id+"_"+f.ahcResponse.getResponseBody.replace("[","").replace("]","").split(",")(0).replace("\"","").toUpperCase().replace("BOOK","RESERVE").replace("ROBO","ROBOT").replace("MESA","TABLE")
+        }
+      }
+      futu map {
+        x => x
+      }
+    }else{
+      Future("")
+    }
   }
+
+
   /*def formatPretty(bigline:String):Future[String] ={
     val id = bigline.split(",")(0)
     val desc = bigline.split(",")(1)
--- app/testingcsv.sc
+++ app/testingcsv.sc
@@ -1,34 +1,30 @@
-import play.api.libs.ws.WS
-
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-val li = "33,  *  zzENetPlc[6].TimeoutFault  Falha de TimeOut Plc 06 Remoto                  /"
 
 
-def formatPretty(bigline:String):Future[String] ={
-  val id = bigline.split(",")(0)
-  val desc = bigline.split(",")(1).replace(".","_").replace(" ","_")
+val li = "249;Reserve - zzSeq[5].Fault[7].25"
 
-  def conver2Letters(line:String):String = {
-    val pattern = "^[ña-zÑA-Z]+$"
-    val splitL = line.split("_")
-    def choice(l:List[String],total:List[String]):String = {
-      if(l.isEmpty) total.mkString(" ") else if(l.head matches(pattern)) choice(l.tail,total:+l.head)
-      else{ choice(l.tail,total)}}
-    choice(splitL.toList,List.empty).toUpperCase()
-  }
 
-  val out = conver2Letters(desc)
+def formatPretty(biglineIn:String):String ={
+  if (biglineIn.split(";").length > 1){
+    val bigline =if(biglineIn.split(";").length==1) biglineIn+"FAULT" else biglineIn
+    val id = bigline.split(";")(0)
+    val desc = bigline.split(";")(1).replace(".","_").replace(" ","_")
 
-  WS.url(s"http://translate.google.com/translate_a/t?client=t&text=$out&hl=pt&sl=pt&tl=en&multires=1&otf=2&pc=1&ssel=0&tsel=0&sc=1".replace(" ","%20").replace("|","%7C")).get().map{
-    //f => s"X=$id;#$id"+"_"+(f.json \ "responseData" \ "translatedText").as[String]
-    f => {
-      s"X=$id;#$id"+"_"+f.ahcResponse.getResponseBody.replace("[","").replace("]","").split(",")(0).replace("\"","").toUpperCase()
+    def conver2Letters(line:String):String = {
+      val pattern = "^[ña-zÑA-Z]+$"
+      val splitL = line.replaceAll("[^ña-zÑA-Z]"," ").replace(" ","_").split("_")
+      def choice(l:List[String],total:List[String]):String = {
+        if(l.isEmpty) total.mkString(" ") else if(l.head matches(pattern)) choice(l.tail,total:+l.head)
+        else{ choice(l.tail,total)}}
+      choice(splitL.toList,List.empty).toUpperCase()
     }
-  }
 
+    val o = conver2Letters(desc)
+    val out = if(o.trim.isEmpty) "FAULT" else o
+    s"X=$id;#$id"+"_"+out
+
+  }else{
+    ""
+  }
 }
 
 formatPretty(li)
-
-
