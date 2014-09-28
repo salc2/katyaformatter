272f8f998831f37c07011041a2fbd110a2a0983b
javier salcedo <javiersalcedo75@gmail.com>
2014-09-28 12:20:01 -0300

    news features

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import java.io.File
 import java.nio.charset.CodingErrorAction
 import java.util.UUID
 
@@ -10,42 +11,40 @@ import play.api.mvc._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scalax.io.{Output, Resource}
 
 object Application extends Controller {
+
 
   def index = Action {
     Ok(views.html.index("KatyaFormatter"))
   }
 
 
- /* def upload = Action.async(parse.multipartFormData) { request =>
-    val csv =request.body.file("csv").get
-    val output:Output = Resource.fromFile("/tmp/ready-"+csv.filename)
-    val futu = scala.concurrent.future{
-      val B = new StringBuilder
-      val filename = csv.filename
-      val contentType = csv.contentType
-      val source = scala.io.Source.fromFile(csv.ref.file)(io.Codec.ISO8859)
-      source.getLines.toList map(x => {
-        if(!x.trim.isEmpty){ formatPretty(x) map {x =>  output.write( x +"\n" ) (scalax.io.Codec.ISO8859) }}
-      })
-      source.close()
-      B.toString()
-    }
-    futu map {
-      x => Ok.sendFile(
-        content = new java.io.File("/tmp/ready-"+csv.filename),
-        fileName = _ => "readys/ready-"+csv.filename
-      )
-    }
+ def indexDownload(fileName:String,col:String) = Action {
+   Ok(views.html.indexDownload(fileName,col))
+ }
 
+  def download = Action{ implicit request =>
+   val uuii = request.body.asFormUrlEncoded.get("uuii").toList.head
+    val fileName = request.body.asFormUrlEncoded.get("fileName").toList.head
+    val collec:MongoCollection = MongoClient ("localhost")("dbfile")(uuii)
+    val file = new File("/tmp/"+uuii+fileName)
+    file.createNewFile()
+    val f:Output = Resource.fromFile("/tmp/"+uuii+fileName)
+    collec foreach (doc => f.write(doc.get("line").toString+"\n"))
+    collec.drop()
+    Ok.sendFile(
+      content = new java.io.File("/tmp/"+uuii+fileName),
+      fileName = _ => fileName.replace(".","-PRO.")
+    )
   }
-*/
+
   def upload = Action.async(parse.multipartFormData) { request =>
     val csv =request.body.file("csv").get
    val uuii = "files"+UUID.randomUUID().toString.replace("-","").substring(0,4)
     //val output:Output = Resource.fromFile(File.separator+"tmp"+File.separator+csv.filename)
-   val collec:MongoCollection = MongoClient("localhost")("dbfiles")(uuii)
+    val collec:MongoCollection = MongoClient ("localhost")("dbfile")(uuii)
       val futu = scala.concurrent.future{
         val filename = csv.filename
         val contentType = csv.contentType
@@ -67,47 +66,10 @@ object Application extends Controller {
         }
       }
       futu map {
-       // val output:Output = Resource.fromFile("/tmp/"+csv.filename)
-       // for (inv <- collec) yield output.write(inv.get("line").toString+"\n")
-        x => Ok(uuii)/*sendFile(
-          content = new java.io.File("/tmp/"+csv.filename),
-          fileName = _ => uuii
-        )*/
+        x =>Redirect(routes.Application.indexDownload(csv.filename,uuii)) //Ok(uuii+", "+csv.filename)
     }
 
   }
-
-
-/*  def formatPretty(biglineIn:String):Future[String] ={
-    if (biglineIn.split(";").length > 1){
-      val bigline =if(biglineIn.split(";").length==1) biglineIn+"FAULT" else biglineIn
-    val id = bigline.split(";")(0)
-    val desc = bigline.split(";")(1).replace(".","_").replace(" ","_")
-
-    def conver2Letters(line:String):String = {
-      val pattern = "^[ña-zÑA-Z]+$"
-      val splitL = line.split("_")
-      def choice(l:List[String],total:List[String]):String = {
-        if(l.isEmpty) total.mkString(" ") else if(l.head matches(pattern)) choice(l.tail,total:+l.head)
-        else{ choice(l.tail,total)}}
-      choice(splitL.toList,List.empty).toUpperCase()
-    }
-
-    val o = conver2Letters(desc)
-    val out = if(o.trim.isEmpty) "FAULT" else o
-    s"X=$id;#$id"+"_"+out
-    WS.url(s"http://translate.google.com/translate_a/t?client=t&text=$out&hl=pt&sl=pt&tl=en&multires=1&otf=2&pc=1&ssel=0&tsel=0&sc=1".replace(" ","%20").replace("|","%7C")).get().map{
-      //f => s"X=$id;#$id"+"_"+(f.json \ "responseData" \ "translatedText").as[String]
-      f => {
-        s"X=$id;#$id"+"_"+f.ahcResponse.getResponseBody.replace("[","").replace("]","").split(",")(0).replace("\"","").toUpperCase()
-      }
-    }
-    }else{
-      Future("")
-    }
-  }*/
-
-
 
 
   def formatPrettyOnline(bigline:String,collec:MongoCollection):Future[String] ={
@@ -116,20 +78,21 @@ object Application extends Controller {
       val tail = if(lsl.tail.isEmpty) "FAULT" else lsl.tail.head
       val id = lsl.head
       val desc = tail.replace(".","_").replace(" ","_")
+      val dessp = if(!desc.split("""\/""").toList.isEmpty){desc.split("""\/""").toList.head}else{desc}
       def conver2Letters(line:String):String = {
-        val pattern = "^[ña-zÑA-Z]+$"
-        val splitL = line.replaceAll("[^ña-zÑA-Z]"," ").replace(" ","_").split("_")
+        val pattern = "^[ña-zÑA-Z0-9]+$"
+        val splitL = line.replaceAll("[^ña-zÑA-Z0-9]"," ").replace(" ","_").split("_")
         def choice(l:List[String],total:List[String]):String = {
           if(l.isEmpty) total.mkString(" ") else if(l.head matches(pattern)) choice(l.tail,total:+l.head)
           else{ choice(l.tail,total)}}
         choice(splitL.toList,List.empty).toUpperCase()
       }
 
-    val o = conver2Letters(desc)
-    val out = if(o.trim.isEmpty) "FAULT" else o
+    val o = conver2Letters(dessp)
+    val out = if(o.trim.isEmpty) "FAULT".toLowerCase else o.toLowerCase
     WS.url(s"http://translate.google.com/translate_a/t?client=t&text=$out&hl=pt&sl=pt&tl=en&multires=1&otf=2&pc=1&ssel=0&tsel=0&sc=1".replace(" ","%20").replace("|","%7C")).get().map{
       f => {
-        val s = "X="+id+";#"+id+"_"+f.ahcResponse.getResponseBody.replace("[","").replace("]","").split(",")(0).replace("\"","").toUpperCase().replace("ROBO","ROBOT").replace("MESA","TABLE").replace("BOOK","RESERVE")
+        val s = "X="+id+";#"+id+"_"+f.ahcResponse.getResponseBody.replace("[","").replace("]","").split(",")(0).replace("\"","").toUpperCase().replace("ROBO","ROBOT").replace("MESA","TABLE").replace("BOOK","RESERVE").replace("PECA","PART").toUpperCase
         collec.save(MongoDBObject("line"->s))
         s
       }
@@ -141,22 +104,22 @@ object Application extends Controller {
 
 
   def formatPrettyOffline(bigline:String,collec:MongoCollection):String = {
-
     val lsl = bigline.split("""\;""").toList
     if(lsl.isEmpty){""}else{
       val tail = if(lsl.tail.isEmpty) "FAULT" else lsl.tail.head
       val id = lsl.head
       val desc = tail.replace(".","_").replace(" ","_")
+      val dessp = if(!desc.split("""\/""").toList.isEmpty){desc.split("""\/""").toList.head}else{desc}
       def conver2Letters(line:String):String = {
-        val pattern = "^[ña-zÑA-Z]+$"
-        val splitL = line.replaceAll("[^ña-zÑA-Z]"," ").replace(" ","_").split("_")
+        val pattern = "^[ña-zÑA-Z0-9]+$"
+        val splitL = line.replaceAll("[^ña-zÑA-Z0-9]"," ").replace(" ","_").split("_")
         def choice(l:List[String],total:List[String]):String = {
           if(l.isEmpty) total.mkString(" ") else if(l.head matches(pattern)) choice(l.tail,total:+l.head)
           else{ choice(l.tail,total)}}
         choice(splitL.toList,List.empty).toUpperCase()
       }
 
-      val o = conver2Letters(desc)
+      val o = conver2Letters(dessp)
       val out = if (o.trim.isEmpty) "FAULT" else o
       val s = "X=" + id + ";#" + id + "_" + out
       collec.save(MongoDBObject("line"->s))
--- app/testingcsv.sc
+++ app/testingcsv.sc
@@ -1,7 +1,27 @@
-import com.mongodb.casbah.MongoClient
-import com.mongodb.casbah.commons.MongoDBObject
 
-val conn = MongoClient("localhost")("test")
+val g = "231;  .  _1A514S.Channel            Portao Acesso 14 Fechado                        /     367.4"
 
-conn("somes").save(MongoDBObject("name" -> "whatever"))
+def formatPrettyOffline(bigline:String):String = {
+  val lsl = bigline.split("""\;""").toList
+  if(lsl.isEmpty){""}else{
+    val tail = if(lsl.tail.isEmpty) "FAULT" else lsl.tail.head
+    val id = lsl.head
+    val desc = tail.replace(".","_").replace(" ","_")
+    val dessp = if(!desc.split("""\/""").toList.isEmpty){desc.split("""\/""").toList.head}else{desc}
+    def conver2Letters(line:String):String = {
+      val pattern = "^[ña-zÑA-Z0-9]+$"
+      val splitL = line.replaceAll("[^ña-zÑA-Z0-9]"," ").replace(" ","_").split("_")
+      def choice(l:List[String],total:List[String]):String = {
+        if(l.isEmpty) total.mkString(" ") else if(l.head matches(pattern)) choice(l.tail,total:+l.head)
+        else{ choice(l.tail,total)}}
+      choice(splitL.toList,List.empty).toUpperCase()
+    }
 
+    val o = conver2Letters(dessp)
+    val out = if (o.trim.isEmpty) "FAULT" else o
+    val s = "X=" + id + ";#" + id + "_" + out
+    s
+  }
+}
+
+formatPrettyOffline(g)
